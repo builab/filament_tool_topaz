1f70f62b2c0584d8d52954259db2fc6883aa3498
builab <huy.bui@mcgill.ca>
2022-02-23 11:23:43 -0500

    Update star_to_anything.py

--- src/star_to_anything.py
+++ src/star_to_anything.py
@@ -1,7 +1,9 @@
 from utils.star_parser import star3_to_topaz
+import os, shutil
 
 pixelSize = 2.74 # Important
 boxSize = 256 # Not so important
+outputDir = "box_g2"
 
 # Important to check column 3,4,6,7 of the star file 
 # Column 3 = CoordinateX
@@ -9,7 +11,16 @@ boxSize = 256 # Not so important
 # Column 6 = rlnOriginXAngst
 # Column 7 = rlnOriginYAngst
 
-parser = star3_to_topaz("group2_P13_J38_bin2.star", "box_g2", [3,4], [3,4,6,7], pixelSize, name_index = 2, optional_attributes = [boxSize,boxSize,-6,], header = "x_coord\ty_coord\tbox_x\tbox_y\tscore\n")
+parser = star3_to_topaz("group2_P13_J38_bin2.star", outputDir, [3,4], [3,4,6,7], pixelSize, name_index = 2, optional_attributes = [boxSize,boxSize,-6,], header = "x_coord\ty_coord\tbox_x\tbox_y\tscore\n")
 
 parser.parse()
 
+coordList = glob.glob(outputDir)
+
+for file in coordList:
+  fileName = os.path.basename(file)
+  filePath = os.path.dirname(file)
+  newFile = fileName[23,:]
+  newFile = filePath + newFileName
+  print(newFile)
+  #shutil.move(file, newFile)
